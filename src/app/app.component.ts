69ed366ec79a62ca6394231893fc78cf7d17fe4e
Elizabeth H Burgess <heb264@gmail.com>
2018-01-01 12:28:24 +0800

    subject from rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LOCATION_INITIALIZED } from '@angular/common';
+import { UsersService } from './user/users.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  user1Activated = false;
+  user2Activated = false;
+
+  constructor(private usersSvc: UsersService ){
+
+  }
+
+  ngOnInit(){
+    this.usersSvc.userActivated.subscribe(
+      (id: number) => {
+        if(id === 1){
+          this.user1Activated = true;
+        }else if (id === 2){
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
 }
